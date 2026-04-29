34576eaccbead8f15b3cdbf3482dd10457177c3e
litterthanlit <nikolay96g@gmail.com>
2026-04-29 23:18:24 +0300

    Allow public beta request route

--- hypher-web/middleware.ts
+++ hypher-web/middleware.ts
@@ -3,6 +3,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isPublicRoute = createRouteMatcher([
   "/",
   "/pricing",
+  "/beta/request",
   "/capture",
   "/app/p/(.*)",
   "/share/s/(.*)",
